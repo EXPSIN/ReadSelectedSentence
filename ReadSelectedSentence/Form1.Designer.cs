4c82c36d79639c9a683e8883d4dcc5a570e61eb8
expsin <wusixstx@163.com>
2023-12-19 15:25:45 +0800

    丐版点读机
    丐版点读机

---
+++ ReadSelectedSentence/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ReadSelectedSentence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选中想要念出的文字，按ctrl+c复制到剪贴板即可念出";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 212);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 263);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ReadSelectedSentence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox textBox1;
+    }
+}
+
---
+++ ReadSelectedSentence/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace ReadSelectedSentence
+{
+    public partial class Form1 : Form
+    {
+        static string last_data = "";
+        // [STAThread] // 必须添加这个属性，确保在单线程单元 (STA) 中执行
+        public Form1()
+        {
+            InitializeComponent();
+            
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // 获取剪贴板的文本内容
+            bool flag = Clipboard.ContainsText();
+            if (flag == true)
+            {
+                string data = Clipboard.GetText();
+                if (last_data != data)
+                {
+                    last_data = data;
+                    textBox1.Text = data;
+
+                    // 创建语音合成器对象
+                    using (SpeechSynthesizer synthesizer = new SpeechSynthesizer())
+                    {
+                        // 设置语音合成器的声音
+                        // synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+                        // synthesizer.SelectVoice("Microsoft Huihui");
+
+
+                        // 调用 Speak 方法进行文本到语音的转换
+                        synthesizer.Speak(data);
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
